4909625067c05088cd794f548e8bed81eaafc940
gabriel <gabriel@okteto.com>
2022-03-29 13:25:06 +0200

    verify breakpoint

--- app.py
+++ app.py
@@ -6,17 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    msg = 'Hello World!'
-    return msg
-
-def attach():
-  if os.environ.get('WERKZEUG_RUN_MAIN'):
-    print('Connecting to debugger...')
-    pydevd_pycharm.settrace('0.0.0.0', port=9000, stdoutToServer=True, stderrToServer=True)
+  msg = 'Hello World!'
+  return msg
 
 if __name__ == '__main__':
   print('Starting hello-world server...')
-  # comment out to use Pycharm's remote debugger
-  # attach()
-
+  pydevd_pycharm.settrace('localhost', port=9000, stdoutToServer=True, stderrToServer=True, suspend=False)
   app.run(host='0.0.0.0', port=8080)
